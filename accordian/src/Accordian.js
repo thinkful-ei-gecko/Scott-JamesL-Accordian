837d0d40e58cb1523a9bb39437aac86f1084a68a
James Lee <jameswlee109@gmail.com>
2019-09-05 13:45:19 -0500

    functionality complete

---
+++ accordian/src/Accordian.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export default class Accordian extends React.Component{
+  state = {
+    currentIndex: null
+  };
+
+  handleButtonClick = index => {
+    if(index === this.state.currentIndex){
+      this.setState({
+        currentIndex: null
+      })
+    }else{
+      this.setState({
+        currentIndex: index
+      })
+    }
+  }
+
+  renderButtons() {
+    return this.props.sections.map((section, index) => (
+      <li key = {index}>
+        <button onClick={() => this.handleButtonClick(index)}>
+          {section.title}
+        </button>
+        <p>{this.state.currentIndex === index ? section.content : ''}</p>
+      </li>
+    ))
+  }
+
+  generateLi(){
+    //return arr of items to gen li elems
+
+  }
+
+
+  render() {
+
+    return (
+      <ul>
+        {this.renderButtons()}
+      </ul>
+    )
+  }
+
+
+}
